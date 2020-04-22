e9759676ecffb286f3f77121cb4674448c81bad5
Vicente Lubascher <velubascher@uc.cl>
2020-04-22 11:12:52 -0400

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ingrediente(models.Model):
+    nombre = models.TextField()
+    descripcion = models.TextField()
+
+
+class Hamburguesa(models.Model):
+    nombre = models.TextField()
+    descripcion = models.TextField()
+    imagen = models.URLField()
+    ingredientes = models.ManyToManyField(Ingrediente)
