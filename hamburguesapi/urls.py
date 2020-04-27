faeffd90f1e676868d140fa4e500ed9b1a5a612e
Vicente Lubascher <velubascher@uc.cl>
2020-04-27 11:28:51 -0400

    fix trailing slash

--- hamburguesapi/urls.py
+++ hamburguesapi/urls.py
@@ -19,7 +19,7 @@ from rest_framework.routers import DefaultRouter
 from api.views import HamburguesaViewSet, IngredienteViewSet
 
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'hamburguesa', HamburguesaViewSet, basename='hamburguesa')
 router.register(r'ingrediente', IngredienteViewSet)
 
