d98e53059461cba49f2b5eb4f7721f55ebbbcf18
Vicente Lubascher <velubascher@uc.cl>
2020-04-27 11:58:41 -0400

    delete hamburguesa response 200

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .serializers import HamburguesaSerializer, IngredienteSerializer
 from .models import Hamburguesa, Ingrediente
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
@@ -22,5 +22,10 @@ class HamburguesaViewSet(viewsets.ModelViewSet):
         hamburguesa = self.get_object()
         serializer = self.get_serializer(hamburguesa)
         return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        hamburguesa = self.get_object()
+        self.perform_destroy(hamburguesa)
+        return Response(status=status.HTTP_200_OK)
 
 
